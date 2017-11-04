3c4b45a3511667d21a91a7d9574a33f1092f29f1
Sourabh Bhagat <sourabh@Sourabhs-MacBook-Pro.local>
2017-11-04 03:00:44 -0400

    added test handler and route

--- main.go
+++ main.go
@@ -4,26 +4,36 @@ import (
 	"fmt"
 	"github.com/unrolled/render"
 	"net/http"
+	"github.com/UBHackathonManagement/participant"
 )
 
-//TestRoute - handler to get expense by ID
-func TestRoute(w http.ResponseWriter, r *http.Request) {
+//GetTeamByName - handler to get team details by name
+func GetTeamByName(w http.ResponseWriter, r *http.Request) {
 	render := render.New()
 
-	expenseID := r.URL.Query().Get("expenseID")
+	teamName := r.URL.Query().Get("teamName")
 
-	expenseObject, err := model.GetExpense(expenseID)
+	expenseObject, err := participant.GetParticipant(teamName)
 	if err != nil {
-		fmt.Println("Cannot Save expense ", err.Error())
-		render.JSON(w, http.StatusBadGateway, "Saved Expense ")
+		fmt.Println("Cannot get Team Name ", err.Error())
+		render.JSON(w, http.StatusBadGateway, "Team Details")
 		return
 	}
 	render.JSON(w, http.StatusOK, expenseObject)
 	return
 }
 
+
+//TestRoute - test route
+func TestRoute(w http.ResponseWriter, r *http.Request) {
+	render := render.New()
+
+	render.JSON(w, http.StatusOK, nil)
+	return
+}
+
 func main() {
 	fmt.Println("Started UB Hackathon Management....")
-	http.HandleFunc("/testRoute", TestRoute)
+	http.HandleFunc("/test", TestRoute)
 	http.ListenAndServe(":8889", nil)
 }
